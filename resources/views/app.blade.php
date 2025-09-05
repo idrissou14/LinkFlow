b61ebb1a17076a03ab6c953f8420ec725a4abd8f
idriss14 <idrissou02nang01@gmail.com>
2025-09-05 11:27:17 +0100

    change name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'LinkHub') }}</title>
+        <title inertia>{{ config('app.name', 'LinkFlow') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
