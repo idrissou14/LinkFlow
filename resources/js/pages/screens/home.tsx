e1478346907026e294cdf2af80b268e1e5d2c746
idriss14 <idrissou02nang01@gmail.com>
2025-09-27 09:13:57 +0100

    ux

--- resources/js/pages/screens/home.tsx
+++ resources/js/pages/screens/home.tsx
@@ -316,7 +316,7 @@ export default function Home() {
           ) : (
             <>
               <Copy size={18} />
-              Copier le lien
+              Copier
             </>
           )}
         </button>
